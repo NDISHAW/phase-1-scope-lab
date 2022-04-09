6253eee602b41b0a45ed99267d5f41902399991e
NDICHU <ndichumuriithi@gmail.com>
2022-04-10 02:59:40 +0300

    commit tree 1882b46e782865d255e10834fd741f4d13fc3043

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+
+var customerName=`bob`
+customerName;
+let leastFavoriteCustomer=`Junkie`
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+}
+//console.log(upperCaseCustomerName());
+
+function setBestCustomer() {
+     bestCustomer=`not bob`
+}
+//console.log(setBestCustomer)
+
+function overwriteBestCustomer(){
+    bestCustomer='maybe bob';
+}
+
+function changeLeastFavoriteCustomer(){
+   let leastFavoriteCustomer = `Kaparo`
+}
+console.log(changeLeastFavoriteCustomer());
